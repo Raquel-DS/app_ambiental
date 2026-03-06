c27f2b00bd9cbe6f6fb2e35440f3f4ce2ec5d26e
Raquel Mattozo <raquelmattozods@gmail.com>
2026-03-05 22:20:51 -0300

    ajuste do projeto

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,40 +1,98 @@
-import { Link } from "expo-router";
-import {Pressable, StyleSheet, Text, View, ScrollView} from 'react-native';
+import { Link, } from "expo-router";
+import {Pressable, StyleSheet, Text, View, ScrollView,} from 'react-native';
+import { Card } from 'react-native-paper';
 import { StatusBar } from "expo-status-bar";
+
 export default function () 
 {
     return (
      <ScrollView>
-          <View style = {styles.container}>
-             <Link style ={styles.links}href="/sobre"> Pressione para ir para a tela Sobre</Link><br/>
-             <Link style ={styles.links}href="/parceria"> Pressione para ir para a tela Parceria</Link><br/>
-             <Link style ={styles.links}href="/relatorio"> Pressione para ir para a tela Relatório</Link><br/>
-             <Link style ={styles.links}href="/sugestoes"> Pressione para ir para a tela Sugestões</Link><br/>
-             <Link style ={styles.links}href="/perfil"> Pressione para ir para a tela Perfil</Link><br/>
-             <Link style ={styles.links}href="/usuarioAnonimo"> Pressione para ir para a tela Anônimo</Link><br/>
-             <Link style ={styles.links}href="/formulario"> Pressione para ir para a tela Formulário</Link><br/>
-             <Link style ={styles.links}href="/cases"> Pressione para ir para a tela Casos de Sucesso</Link><br/>
-             </View>
-     </ScrollView>
-    )
-}
+           <View style = {styles.container}>
+             
+           <View style = {styles.card}>
+             <Link style ={styles.cardtexto}href="/sobre"> Pressione para ir para a tela Sobre</Link><br/>
+       </View>
+        <View style = {styles.container1}></View>
+           <View style = {styles.card1}>
+             <Link style ={styles.cardtexto}href="/parceria"> Pressione para ir para a tela Parceria</Link><br/>
+       </View>
+        <View style = {styles.container}></View>
+            <View style = {styles.card}>
+             <Link style ={styles.cardtexto}href="/relatorio"> Pressione para ir para a tela Relatório</Link><br/>
+       </View>
+            <View style = {styles.card1}>
+             <Link style ={styles.cardtexto}href="/sugestoes"> Pressione para ir para a tela Sugestões</Link><br/>
+       </View>
+       
+       <View style = {styles.card}>
+             <Link style ={styles.cardtexto}href="/perfil"> Pressione para ir para a tela Perfil</Link><br/>
+       </View>
+       <View style = {styles.card1}>
+             <Link style ={styles.cardtexto}href="/usuarioAnonimo"> Pressione para ir para a tela Anônimo</Link><br/>
+       </View>
+       <View style = {styles.container1}></View>
+       <View style = {styles.card}>
+             <Link style ={styles.cardtexto}href="/formulario"> Pressione para ir para a tela Formulário</Link><br/>
+       </View>
+           <View style = {styles.card1}>
+             <Link style ={styles.cardtexto}href="/cases"> Pressione para ir para a tela Casos de Sucesso</Link><br/>
+       </View>
+       </View>
+       </ScrollView>
+            );
+            
+  }
 
 const styles = StyleSheet.create
 ({
     container: {
         flex:1,
-        backgroundColor: '#3525d9',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#c398ceff',
+        alignItems: 'left',
+        justifyContent: 'center', 
+        flexDirection: 'row',  
+        height: 500,
+        wight:"50%",
+      
     },
-    links:{
-        marginTop:20,
-        fontSize:18,
-        color:'#d96b25',
-        backgroundColor: '#082c03ff',
-        paddingTop:15,
-        paddingEnd:15,
-        paddingBottom:15,
-        borderRadius:25,
-    }
+    container1: {
+        flex:1,
+        backgroundColor: '#c398ceff',
+        alignItems: 'left',
+        justifyContent: 'center', 
+        flexDirection: 'column',  
+        height: 100,
+        wight:"50%",
+      
+    },
+     
+card: {
+    backgroundColor: '#66436eff',
+    height: 100,
+    width: "50%",
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 5,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    flexDirection: 'row', 
+    gap:10
+  },
+  card1: {
+    backgroundColor: '#28152cff',
+    height: 100,
+    width: "50%",
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 5,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    flexDirection: 'row', 
+  },
+  cardtexto: {
+    fontSize: 25,
+    color: 'white',
+    fontFamily: 'montserrat',
+  },
 });
+
